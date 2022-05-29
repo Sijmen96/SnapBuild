1d0867169eed131b889f8ca1b7dd7f286e1c27f5
Sijmen <37208493+Sijmen96@users.noreply.github.com>
2022-05-29 15:43:36 +0200

    PlayerController Jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -29,7 +29,10 @@ public class PlayerController : MonoBehaviour
     private Vector3 cameraRotationTarget;
     private Transform cameraPivot;
     private PlayerMovementState movementState = PlayerMovementState.idle;
+    private PlayerJumpState jumpState = PlayerJumpState.grounded;
     private bool grounded;
+    private bool jumpKey;
+    private bool jumpKeyDown;
 
     void Start()
     {
@@ -40,21 +43,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         receivePlayerInput();
-
-        //Check if grounded
-        grounded = Physics.CheckSphere(transform.position + new Vector3(0, -0.6f, 0), .5f, LayerMask.GetMask("Default"));
-
-        if (grounded && this.movementState == PlayerMovementState.jumping)
-        {
-            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            Debug.Log("jumping");
-        }
-
-
     }
 
     void FixedUpdate()
     {
+        jumpHandler();
+
         playerDirectionVector = (cameraPivot.rotation * playerInputVector);
         playerDirectionVector.Normalize();
         playerDirectionVector = new Vector3(playerDirectionVector.x * playerSpeed, playerRigidbody.velocity.y, playerDirectionVector.z * playerSpeed);
@@ -68,10 +62,54 @@ public class PlayerController : MonoBehaviour
         cameraPivot.position = Vector3.Lerp(cameraPivot.position, transform.position, Time.fixedDeltaTime * cameraSmoothing);
     }
 
+
+    void jumpHandler()
+    {
+        Debug.Log(jumpKeyDown);
+        //Debug.Log(this.jumpState);
+        if (this.jumpState == PlayerJumpState.grounded && jumpKeyDown)
+        {
+            jumpKeyDown = false;
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        //Determine jumpstate
+        if (Physics.CheckSphere(transform.position + new Vector3(0, -0.6f, 0), .5f, LayerMask.GetMask("Default")))
+        {
+            this.jumpState = PlayerJumpState.grounded;
+        }
+        else if (playerRigidbody.velocity.y > 0)
+        {
+            this.jumpState = PlayerJumpState.rising;
+        }
+        else if (playerRigidbody.velocity.y < 0)
+        {
+            this.jumpState = PlayerJumpState.falling;
+        }
+
+        if (this.jumpState == PlayerJumpState.falling)
+        {
+            playerRigidbody.velocity += Vector3.up * Physics.gravity.y * jumpFallMultiplier * Time.deltaTime;
+        }
+        else if (this.jumpState == PlayerJumpState.rising && !jumpKey)
+        {
+            playerRigidbody.velocity += Vector3.up * Physics.gravity.y * jumpForceMultiplier * Time.deltaTime;
+        }
+    }
+
+
     void receivePlayerInput()
     {
         //Keybinding
         playerInputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        jumpKey = Input.GetKey(KeyCode.Space);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyDown = true;
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -107,10 +145,16 @@ public class PlayerController : MonoBehaviour
         {
             cameraRotationTarget += new Vector3(0, Input.GetAxis("Mouse X") * cameraSensivity, 0);
         }
+        //Debug.Log(this.movementState);
     }
 }
 
 public enum PlayerMovementState
 {
-    idle = 0, walking = 1, running = 2, crouching = 3, jumping = 4, blocked = 5
+    idle = 0, walking = 1, running = 2, crouching = 3, jumping = 4, blocked = 5, falling = 6
+}
+
+public enum PlayerJumpState
+{
+    rising = 0, falling = 1, grounded = 2
 }
